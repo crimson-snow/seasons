18d51f17b05f1175fc1a1b53d35fb9343686a5e7
Thomas Sullivan <thomas@tsullivan.me>
2018-09-04 16:27:23 -0400

    program rewritten, SFV features removed

--- renametv.py
+++ renametv.py
@@ -1,209 +1,171 @@
 #!/usr/bin/env python3
 
 import os
-import re
 import sys
 import shutil
 import argparse
-import zlib
 from natsort import natsorted, ns
 
-# ANSI color escape codes
-RED = '\033[0;31m'
-GREEN = '\033[0;32m'
-NC = '\033[0m'  # No Color
 
-episodes = {}
-ignoredtypes = {'.sfv', '.DS_Store', '.Spotlight-V100', '.Trashes',
-                '.DocumentRevisions-V100', '.fseventsd',
-                '.VolumeIcon.icns', '.localized'}
+class Series:
+    def __init__(self, seasonnum, title, episodes=None):
+        self.title = title
+        self.seasonnum = seasonnum
+        self.episodes = []
+
+
+class Episode:
+    ignoredtypes = {'.sfv', '.DS_Store', '.Spotlight-V100', '.Trashes',
+                    '.DocumentRevisions-V100', '.fseventsd',
+                    '.VolumeIcon.icns', '.localized', '.gitignore'}
+
+    def __init__(self, title, episodenum,
+                 origfilename, newfilename=None,
+                 source=None, destination=None):
+        self.title = title
+        self.episodenum = episodenum
+        self.origfilename = origfilename
+        self.extension = origfilename.split('.')[1]
+        self.newfilename = newfilename
+        self.source = source
+        self.destination = destination
+
+    def makefilename(self, seasonnum):
+        self.newfilename = (self.title
+                            + ' - S{:02d}'.format(seasonnum)
+                            + 'E{:02d}'.format(self.episodenum)
+                            + '.' + self.extension)
+
+    def __repr__(self):
+        return "Episode('{}', '{}', '{}', '{}')".format(self.title,
+                                                        self.episodenum,
+                                                        self.origfilename,
+                                                        self.newfilename,
+                                                        self.source,
+                                                        self.destination)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Rename many TV show \
-                                                episodes for Plex at once',
+    parser = argparse.ArgumentParser(description='A lightweight organization \
+                                                  tool for your media library',
                                      epilog='The current working \
                                                 directory is the default \
                                                 input and output location')
 
     parser.add_argument('title', metavar='TITLE',
-                        help='define a TV show title')
+                        help='specify a TV show title')
     parser.add_argument('dir', metavar='DIR',  nargs='*', default='.',
-                        help='define a working directory')
+                        help='specify a working directory')
     parser.add_argument('-c', '--copy', action='store_true',
                         help='copy files to output location')
-    parser.add_argument('-e', '--extension',
-                        help='only rename files with specified extension')
-    parser.add_argument('-E', '--episodestart', type=int, default=1,
+    parser.add_argument('-e', '--episodestart', type=int, default=1,
                         help='specify the starting episode number')
-    parser.add_argument('-m', '--makesfv', action='store_true',
-                        help='create an sfv file for each DIR')
     parser.add_argument('-o', '--output',
-                        help='define an output location')
+                        help='specify an output location')
     parser.add_argument('-q', '--quiet', action='store_true',
                         help='surpress prompts and proceed with writing files')
-    parser.add_argument('-r', '--readsfv', action='store_true',
-                        help='read sfv file in DIR to verify files')
     parser.add_argument('-s', '--seasonstart', type=int, default=1,
                         help='specify the starting season number')
     parser.add_argument('--version', action='version',
-                        version='%(prog)s v1.1')
+                        version='%(prog)s v1.1.1')
 
     args = parser.parse_args()
 
-    title = args.title
-    directories = args.dir
-    output = args.output or False
-    seasonnum = args.seasonstart
-    episodenum = args.episodestart
-    extension = args.extension
+    options = {
+        'title': args.title,
+        'directories': args.dir,
+        'copy': args.copy,
+        'quiet': args.quiet,
+        'output': args.output or False,
+        'seasonnum': args.seasonstart,
+        'episodenum': args.episodestart,
+    }
 
-    for directory in directories:
-        print(directory)
-        if args.readsfv:
-            sfv = findsfv(directory)
-            if sfv:
-                sfvparsed = sfvparse(sfv)
-            else:
-                sfvparsed = False
+    series = Series(options['seasonnum'], options['title'])
+
+    for directory in options['directories']:
+        directory = directory.rstrip('/')
+        if not options['output']:
+            options['output'] = directory
+
+        for file in [x for x in natsorted(os.listdir(directory),
+                     alg=ns.IGNORECASE)
+                     if not os.path.splitext(x)[0] in Episode.ignoredtypes
+                     if not os.path.splitext(x)[1] in Episode.ignoredtypes
+                     if not os.path.isdir(os.path.join(directory, x))]:
+
+            newep = Episode(series.title, options['episodenum'], file)
+
+            newep.makefilename(series.seasonnum)
+            newep.source = directory
+            newep.destination = '{}/Season {:02d}'.format(options['output'],
+                                                          series.seasonnum)
+
+            series.episodes.append(newep)
+
+            print('[{}] from [{}/{}] to [{}/{}]'.format(mode(options['copy']),
+                                                        newep.source,
+                                                        newep.origfilename,
+                                                        newep.destination,
+                                                        newep.newfilename))
+
+            options['episodenum'] += 1
+
+        if options['quiet'] or userprompt():
+            writefiles(series.episodes, options['copy'])
         else:
-            sfvparsed = False
-
-        if output is False:
-            output = directory
-
-        createepisodenames(title, directory, output,
-                           seasonnum, episodenum, extension, sfvparsed)
-        seasonnum += 1
-        domakesfv = verifychanges(args.quiet, args.copy)
-
-        if args.makesfv and domakesfv:
-            makesfv(output)
-
-        output = args.output or False
-        episodes.clear()
-
-
-def findsfv(src):
-    sfvfiles = []
-    for file in [x for x in os.listdir(src)
-                 if os.path.splitext(x)[1] == ".sfv"]:
-        sfvfiles.append(src + '/' + file)
-
-    if sfvfiles:
-        return sfvfiles[0]
-    else:
-        return False
-
-
-def makecrc(filepath):
-    f = open(filepath, 'rb')
-    signedcrc = zlib.crc32(f.read()) % (1 << 32)
-    f.close()
-    return format(signedcrc, 'x')
-
-
-def sfvparse(sfv):
-    filelist = []
-    f = open(sfv)
-    for line in f:
-        # parse for comments
-        nocom = re.sub(r'\;.*', '', line)
-        filelist.append(tuple(nocom.strip().rsplit(' ', 1)))
-
-    f.close()
-    return filelist
-
-
-def makesfv(directory):
-    sfvname = directory + '/' + os.path.basename(directory) + '.sfv'
-    f = open(sfvname, 'a')
-
-    for e in episodes:
-        file = episodes[e]['new']['file']
-        path = episodes[e]['new']['path']
-        f.write(file + ' ' + makecrc(path + file) + '\n')
-
-    f.close()
-    print('Wrote to ' + sfvname)
-
-
-def crcvalidate(crc1, crc2):
-    return (crc1.lower() == crc2.lower())
-
-
-def makechanges(copy):
-    for e in episodes:
-        oldpath = episodes[e]['old']['path'] + episodes[e]['old']['file']
-        newpath = episodes[e]['new']['path'] + episodes[e]['new']['file']
-
-        if not os.path.exists(episodes[e]['new']['path']):
-                os.makedirs(episodes[e]['new']['path'])
-
-        if copy:
-            try:
-                print('Copying ' + '"' + episodes[e]['new']['file'] + '"'
-                      + ' to ' + episodes[e]['new']['path'])
-                shutil.copy(oldpath, newpath)
-            except shutil.SameFileError:
-                print('Cannot copy. ',
-                      'File already exists at the desired output location.')
-
-        else:
-            print('Moving ' + '"' + episodes[e]['new']['file']
-                  + '"' + ' to ' + episodes[e]['new']['path'])
-            shutil.move(oldpath, newpath)
-
-
-def verifychanges(quiet, copy):
-    while True:
-        userinput = quiet or \
-                    input('Write files? ((y)es, (n)o, (q)uit) ').lower()
-
-        if userinput == 'y' or userinput is True:
-            makechanges(copy)
-            print('Done!')
-            return True
-        elif userinput == 'n':
-            print('No changes made.\n')
-            return False
-        elif userinput == 'q':
-            print('Exiting...')
             sys.exit()
+
+        # Reset values for next directory
+        series.episodes.clear()
+        options['episodenum'] = 1
+        series.seasonnum += 1
+
+
+def writefiles(episodes, copy):
+    for e in episodes:
+        if not os.path.exists(e.destination):
+                os.makedirs(e.destination)
+        mode = 'COPYING' if copy else 'MOVING'
+        try:
+            print('[{}] from [{}/{}] to [{}/{}]'.format(mode,
+                                                        e.source,
+                                                        e.origfilename,
+                                                        e.destination,
+                                                        e.newfilename))
+            if copy:
+                shutil.copy(e.source + '/' + e.origfilename,
+                            e.destination + '/' + e.newfilename)
+            else:
+                shutil.move(e.source + '/' + e.origfilename,
+                            e.destination + '/' + e.newfilename)
+
+        except shutil.SameFileError:
+                print('Cannot {}}. File already exists at the desired \
+                      output location.'.format(mode(copy).lower()))
+
+
+def userprompt():
+    while True:
+        userinput = input('Write files? [(y)es, (n)o, (q)uit]').lower()
+
+        if userinput == 'y':
+            return True
+        elif userinput == 'n' or userinput == 'q':
+            print('No changes made.')
+            return False
         else:
             print('Please enter a valid option.')
 
+        print('\n')
 
-def createepisodenames(title, src, dst, season,
-                       episode, extension, sfv):
-    for file in [x for x in natsorted(os.listdir(src), alg=ns.IGNORECASE)
-                 if not os.path.splitext(x)[0] in ignoredtypes
-                 if not os.path.splitext(x)[1] in ignoredtypes
-                 if not os.path.isdir(os.path.join(src, x))]:
-        if extension:
-            if extension[0] != '.':
-                ext = '.' + extension
-            else:
-                ext = extension
-        else:
-            _, ext = os.path.splitext(file)
 
-        newname = (title + ' - S{:02d}'.format(season) +
-                   'E{:02d}'.format(episode) + ext)
-        print(file + ' --> ' + newname)
-
-        if sfv:
-            for t in sfv:
-                if file in t:
-                    if crcvalidate(makecrc(src + '/' + file),
-                                   t[1].strip('\n')):
-                        print(GREEN + '[CRC Match]' + NC)
-                    else:
-                        print(RED + '[CRC Mismatch]' + NC)
-
-        episodes[episode] = {'old': {'path': src + '/', 'file': file},
-                             'new': {'path': dst + '/', 'file': newname}}
-        episode += 1
+def mode(argument):
+    switcher = {
+        0: "MOVE",
+        1: "COPY"
+    }
+    return switcher.get(argument, "nothing")
 
 
 if __name__ == '__main__':
